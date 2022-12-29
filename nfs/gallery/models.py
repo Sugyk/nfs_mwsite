9cc3cc5fc8af2e9cc5981ce2da18d1131f7bbcc5
Sugar <war2015n2@mail.ru>
2022-12-29 16:40:20 +0300

    registrated gallery and created models

--- nfs/gallery/models.py
+++ nfs/gallery/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    title = models.CharField(verbose_name='Название бренда')
+    country = models.CharField(verbose_name='Страна производитель')
+    description = models.TextField(null=True, verbose_name='Описание')
+    logo = models.ImageField(verbose_name='Логотип')
+
+
+class Car(models.Model):
+    brand = models.ForeignKey(verbose_name='Марка')
+    photo = models.ImageField(verbose_name='Фотография авто')
+    model = models.CharField(verbose_name='Модель')
+    model_year = models.IntegerField(verbose_name='Год выпуска')
+
+
+class CarNote(models.Model):
+    title = models.CharField(null=True, verbose_name='Описание')
+    position = models.IntegerField(verbose_name='Позиция текста')
+    content = models.TextField(verbose_name='Содержание')
+
+
+class CarImage(models.Model):
+    position = models.IntegerField(verbose_name='Позиция картинки')
+    image = models.ImageField(verbose_name='Картинка')
+    description = models.CharField(null=True, verbose_name='Описание')
