52fdf2426f8f0ea776a3e9cc23d4351dfdb28ba0
Sugar <war2015n2@mail.ru>
2022-12-30 21:45:26 +0300

    static files configuration

--- nfs/nfs/settings.py
+++ nfs/nfs/settings.py
@@ -119,6 +119,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    'D:\Projects\ClearPicturesShop\webshop\static\main_static.css',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
