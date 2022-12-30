747cbc6b8e09a989a62f0e725e11645102cae3b3
Sugar <war2015n2@mail.ru>
2022-12-30 21:37:33 +0300

    registration, login, logut implemented

--- nfs/gallery/urls.py
+++ nfs/gallery/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import CarListView
+from .views import CarListView, UserCreateView, LoginUser, LogoutUser
 
 
 urlpatterns = [
-    path('', CarListView.as_view(), name='car_list')
+    path('', CarListView.as_view(), name='car_list'),
+    path('reg/', UserCreateView.as_view(), name='reg'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', LogoutUser.as_view(), name='logoout'),
 ]
--- nfs/gallery/views.py
+++ nfs/gallery/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 from .models import Car
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'gallery/registration.html'
+    success_url = reverse_lazy('car_list')
 
 
 class CarListView(ListView):
     model = Car
     template_name = 'gallery/car_list.html'
+
+
+class LogoutUser(LogoutView):
+    template_name = 'gallery/logout.html'
+
+
+class LoginUser(LoginView):
+    template_name = 'gallery/login.html'
+    form_class = AuthenticationForm
+    success_url = reverse_lazy('car_list')
