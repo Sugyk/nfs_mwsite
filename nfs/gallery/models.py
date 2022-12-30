3b421d52bb3ee8cdbcd10d5317117bba4ac6cae6
Sugar <war2015n2@mail.ru>
2022-12-30 17:30:58 +0300

    added new models and fixed latter

--- nfs/gallery/models.py
+++ nfs/gallery/models.py
@@ -7,6 +7,9 @@ class Brand(models.Model):
     description = models.TextField(null=True, verbose_name='Описание')
     logo = models.ImageField(verbose_name='Логотип')
 
+    def __str__(self):
+        return self.title
+
 
 class Car(models.Model):
     brand = models.ForeignKey(verbose_name='Марка')
@@ -14,14 +17,25 @@ class Car(models.Model):
     model = models.CharField(verbose_name='Модель')
     model_year = models.IntegerField(verbose_name='Год выпуска')
 
+    def __str__(self):
+        return '{} {}'.format(self.brand, self.model)
+
+
+class CarInfo(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+
 
 class CarNote(models.Model):
-    title = models.CharField(null=True, verbose_name='Описание')
+    note_id = models.ForeignKey(CarInfo, on_delete=models.CASCADE)
     position = models.IntegerField(verbose_name='Позиция текста')
+    title = models.CharField(null=True, verbose_name='Описание')
     content = models.TextField(verbose_name='Содержание')
 
 
 class CarImage(models.Model):
+    note_id = models.ForeignKey(CarInfo, on_delete=models.CASCADE)
     position = models.IntegerField(verbose_name='Позиция картинки')
     image = models.ImageField(verbose_name='Картинка')
     description = models.CharField(null=True, verbose_name='Описание')
