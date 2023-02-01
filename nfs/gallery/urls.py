56462d3bfb3bd6f30b6f5b38e0d47a7e84fa1494
Sugar <war2015n2@mail.ru>
2023-02-02 02:01:17 +0300

    implemented profile page

--- nfs/gallery/urls.py
+++ nfs/gallery/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import CarListView, UserCreateView, LoginUser, LogoutUser, BrandListView
+from .views import CarListView, UserCreateView, LoginUser, LogoutUser, BrandListView, ProfileView
 
 
 urlpatterns = [
     path('', CarListView.as_view(), name='car_list'),
     path('reg/', UserCreateView.as_view(), name='reg'),
     path('login/', LoginUser.as_view(), name='login'),
-    path('logout/', LogoutUser.as_view(), name='logoout'),
-    path('brand/<int:brand_id>', BrandListView.as_view(), name='brand_order')
+    path('logout/', LogoutUser.as_view(), name='logout'),
+    path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
+    path('brand/<int:brand_id>', BrandListView.as_view(), name='brand_order'),
 ]
--- nfs/gallery/views.py
+++ nfs/gallery/views.py
@@ -1,18 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
+from django.conf import settings
 from .models import Car
-
-
-class UserCreateView(CreateView):
-    model = User
-    form_class = UserCreationForm
-    template_name = 'gallery/registration.html'
-    success_url = reverse_lazy('car_list')
 
 
 class CarListView(ListView):
@@ -27,8 +22,16 @@ class BrandListView(ListView):
         return Car.objects.filter(brand=self.kwargs.get('brand_id'))
 
 
-class LogoutUser(LogoutView):
-    next_page = reverse_lazy('login')
+class ProfileView(DetailView):
+    template_name = 'gallery/profile.html'
+    model = User
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'gallery/registration.html'
+    success_url = reverse_lazy('car_list')
 
 
 class LoginUser(LoginView):
@@ -36,3 +39,7 @@ class LoginUser(LoginView):
     form_class = AuthenticationForm
     redirect_authenticated_user = True
     next_page = reverse_lazy('car_list')
+
+
+class LogoutUser(LogoutView):
+    template_name = 'gallery/logout.html'
